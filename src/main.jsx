853e285e99f8cfed87663deae111a9ef7f96471c
Rafi <md.rafi888444@gmail.com>
2025-07-10 00:26:36 +0600

    firebase

--- src/main.jsx
+++ src/main.jsx
@@ -11,13 +11,16 @@ import {
   RouterProvider,
 } from "react-router";
 import { router } from './router/router.jsx';
+import AuthProvider from './contexts/authContext/AuthProvider.jsx';
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <div className='font-urbanist max-w-7xl mx-auto'>
-<RouterProvider router={router} />
+<AuthProvider>
+  <RouterProvider router={router} />
+</AuthProvider>
     </div>
      
   </StrictMode>,
